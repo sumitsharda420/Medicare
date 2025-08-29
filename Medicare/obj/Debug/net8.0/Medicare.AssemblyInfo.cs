eaf3a0fed59324e2c0321cfb88ecd1381167393b
sumit sharda <sumit.sharda69@gmail.com>
2025-08-29 12:47:06 +0530

    Updated Code For Docker

--- Medicare/obj/Debug/net8.0/Medicare.AssemblyInfo.cs
+++ Medicare/obj/Debug/net8.0/Medicare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Medicare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62a494c826a91e9b305298276654d63849cd2e8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Medicare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Medicare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
